b5628f97ff9f200ec85666ae09cfb68e9fb5c18c
Stanislas Plessia <stanislas.plessia@streem.com.au>
2019-04-07 21:34:28 +0200

    Update python scraper

--- pythonscraper/scraper.py
+++ pythonscraper/scraper.py
@@ -36,13 +36,48 @@ def getComicMetadata(link):
         metadata["issues_links"] = [ issue_raw["href"] for issue_raw in issues_raw]
     return metadata
 
+def getIssuePages(link):
+    res = requests.get("{}/full".format(link))
+    soup = bs4.BeautifulSoup(res.text, "html.parser")
+    page_raw = soup.find_all("img", { "class" : "chapter_img"})
+    img_raw = page_raw[0].find_all("img")
+    pages = [ { "page" : raw["alt"] , "src" : raw["src"] } for raw in img_raw ]
+    return pages
 
+def imgDownloader(link, filename):
+    filename = filename.replace(" ", "_").replace("/", "_")
+    print("Downloading ", "{}.jpg".format(filename) )
+    data = requests.get(link, stream=True).content
+    with open("comics/{}.jpg".format(filename), "wb") as handler:
+        handler.write(data)
+
+
+def downloader():
+    kjson = getKeywords("comics.json")
+    for key,value in kjson:
+        for keyword in value:
+            links = getRelevantComicLinks(keyword)
+            for comic in links:
+                metadata = getComicMetadata(comic)
+                for issue in metadata["issues_links"]:
+                    pages = getIssuePages(issue)
+                    for page in pages:
+                        imgDownloader(page["src"], page["page"])
+
+
+# Example
 kjson = getKeywords("comics.json")
+keyword = kjson["DC"][9]
+links = getRelevantComicLinks(keyword)
+metadata = getComicMetadata(links[8])
 
-DCComics = []
-for keywords in kjson["DC"]:
-    comics_link = getRelevantComicLinks(keywords)
-    for link in comics_link:
-        DCComics.append(getComicMetadata(link))
+print(metadata["issues_links"])
+pages = getIssuePages(metadata["issues_links"][0])
+imgDownloader(pages[10  ]["src"], pages[10]["page"])
 
-print(DCComics)
+# DCComics = []
+# for keywords in kjson["DC"]:
+#     comics_link = getRelevantComicLinks(keywords)
+#     for link in comics_link:
+#         DCComics.append(getComicMetadata(link))
+# print(DCComics)
