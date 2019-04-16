6439cfa0a7d98648dfc8ce184797136d29459841
Plessia Stanislas <stplessia@gmail.com>
2019-04-16 20:14:55 +0200

    Supervised Models : Transfer Learnning (#1)
    Supervised Models : Transfer Learning

---
+++ supervised_learning/dataset_model.py
@@ -0,0 +1,60 @@
+from __future__ import print_function, division
+import os, sys
+import torch
+from PIL import Image
+# Allow truncated images
+Image.LOAD_TRUNCATED_IMAGES = True
+
+from skimage import transform
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+
+# Ignore warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+class ComicPageDataset(Dataset):
+    """
+    Comic Page Dataset.
+    """
+    def __init__(self, root_dir, all_comic_images, transform, labels_to_idx):
+        """
+        Args:
+            root_dir (string): Directory with all the images.
+            all_comic_images (list): List of all the existing images
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.to_tensor = transforms.ToTensor()
+        self.root_dir = root_dir
+        self.all_comic_images = all_comic_images
+        self.transform = transform
+        self.labels_to_idx = labels_to_idx
+
+    def __len__(self):
+        return len(self.all_comic_images)
+    
+    def __getitem__(self, idx):
+        img_name = self.all_comic_images[idx]["filename"]
+        with open(os.path.join("..", img_name), 'rb') as f:
+            image = Image.open(f)
+            if self.transform:
+                image = self.transform(image)
+        # comic_name = self.all_comic_images[idx]["comic_name"]
+        
+        publication_year = self.all_comic_images[idx]["label"]
+        if(int(publication_year) < 1954):
+            label = "Golden Age"
+        elif(int(publication_year) < 1970):
+            label = "Silver Age"
+        elif(int(publication_year) < 1986):
+            label = "Bronze Age"
+        else:
+            label = "Modern Age"
+
+        label = self.labels_to_idx[label]
+
+        # img_as_tensor = self.to_tensor(image)
+
+        return (image, label)
---
+++ supervised_learning/load_dataset_train.py
@@ -0,0 +1,38 @@
+from __future__ import print_function, division
+import os, sys
+import torch
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+
+# Ignore warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+
+sys.path.insert(0, os.path.join(".."))
+import pythonscraper.db as db
+sys.path.insert(0, os.path.join("supervised_learning"))
+
+
+from dataset_model import ComicPageDataset
+
+def load_dataset(root_dir, data_transforms, labels_to_idx):
+    files = db.query("SELECT * from files ORDER BY RANDOM() LIMIT 100", ())
+    all_comic_images = []
+    for file in files:
+        new_page = {}
+        new_page["filename"] = file[1]
+        new_page["label"] = file[3]
+        new_page["comic_name"] = new_page["filename"].split("/")[1]
+        all_comic_images.append(new_page)
+
+    comic_page_dataset = ComicPageDataset(root_dir=root_dir, all_comic_images=all_comic_images, transform=data_transforms, labels_to_idx=labels_to_idx)
+
+    return comic_page_dataset
+
+def transform_dataset(dataset, transform):
+    n = len(dataset)
+    for i in range(n):
+        dataset[i]["image"] = transform(dataset[i]["image"])
+    return dataset
