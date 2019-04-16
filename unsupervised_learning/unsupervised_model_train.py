a336613dd3dce6ff001def4057e2163b5a3831a1
Stanislas Plessia <stanislas.plessia@streem.com.au>
2019-04-16 22:10:17 +0200

    UPdate unsupervised

--- unsupervised_learning/unsupervised_model_train.py
+++ unsupervised_learning/unsupervised_model_train.py
@@ -12,6 +12,8 @@ from torchvision import transforms, utils
 import torch.optim as optim
 
 from sklearn.model_selection import train_test_split
+from sklearn.cluster import KMeans
+from sklearn.metrics import accuracy_score, normalized_mutual_info_score
 
 sys.path.insert(0, os.path.join(".."))
 import pythonscraper.db as db
@@ -63,7 +65,7 @@ num_epochs = 5 #you can go for more epochs, I am using a mac
 batch_size = 8
 
 # Create training, validation and test dataloaders
-dataloader = DataLoader(image_datasets["train"], batch_size=batch_size, shuffle=True, num_workers=4) 
+dataloaders_dict = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size, shuffle=True, num_workers=4) for x in ["train", "val", "test"]}
 
 ae = AutoEncoder().cuda()
 distance = nn.MSELoss()
@@ -72,12 +74,47 @@ optimizer = optim.Adam(ae.parameters(), lr=0.001)
 ae.train()
 
 for epoch in range(num_epochs):
-    for data in dataloader:
-        inputs, labels = data
-        inputs = Variable(inputs).cuda()
-        output = ae(inputs)
-        loss = distance(output, inputs)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-    print('epoch [{}/{}], loss:{:.4f}'.format(epoch+1, num_epochs, loss.item()))
+    for phase in [ "train", "val", "test" ]:
+
+        if phase == "train":
+            ae.train()   # Set ae to training mode
+        elif phase == "val":
+            ae.eval()    # Set ae to evaluate mode
+        else:
+            ae.eval()   
+
+        for data in dataloaders_dict[phase]:
+            inputs, labels = data
+            inputs = Variable(inputs).cuda()
+            output = ae(inputs)
+            loss = distance(output, inputs)
+            optimizer.zero_grad()
+            if phase == "train":
+                loss.backward()
+                optimizer.step()
+        print('{} epoch [{}/{}], loss:{:.4f}'.format(phase, epoch+1, num_epochs, loss.item()))
+
+train_x = []
+train_y = []
+val_x = []
+val_y = []
+for data in dataloaders_dict["train"]:
+    inputs, labels = data
+    train_x.append(Variable(inputs).cuda())
+    train_y.append(labels)
+
+for data in dataloaders_dict["val"]:
+    inputs, labels = data
+    val_x.append(Variable(inputs).cuda())
+    val_y.append(labels)
+
+pred_auto_train = [ ae.encoder(inp) for inp in train_x ]
+pred_auto = [ ae.encoder(inp) for inp in val_x ]
+
+km = KMeans(n_jobs=-1, n_clusters=4, n_init=20)
+
+km.fit(pred_auto_train)
+pred = km.predict(pred_auto)
+
+normalized_mutual_info_score(val_y, pred)
+print(pred)
