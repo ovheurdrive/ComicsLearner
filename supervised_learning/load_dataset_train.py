f91aaceca294cd40c2611757701ed191765ab004
Stanislas Plessia <stanislas.plessia@streem.com.au>
2019-04-16 20:42:07 +0200

    Add dataloader in autoencoder train

--- supervised_learning/load_dataset_train.py
+++ supervised_learning/load_dataset_train.py
@@ -15,7 +15,7 @@ import pythonscraper.db as db
 sys.path.insert(0, os.path.join("supervised_learning"))
 
 
-from dataset_model import ComicPageDataset
+from supervised_learning.dataset_model import ComicPageDataset
 
 def load_dataset(root_dir, data_transforms, labels_to_idx):
     files = db.query("SELECT * from files ORDER BY RANDOM() LIMIT 100", ())
--- unsupervised_learning/unsupervised_model_train.py
+++ unsupervised_learning/unsupervised_model_train.py
@@ -11,6 +11,13 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 import torch.optim as optim
 
+from sklearn.model_selection import train_test_split
+
+sys.path.insert(0, os.path.join(".."))
+import pythonscraper.db as db
+sys.path.insert(0, os.path.join("supervised_learning"))
+from supervised_learning.load_dataset_train import load_dataset, transform_dataset
+
 
 # Ignore warnings
 import warnings
@@ -23,8 +30,6 @@ sys.path.insert(0, os.path.join("unsupervised_learning"))
 from autoencoder import AutoEncoder
 import pythonscraper.db as db
 
-transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.4914, 0.4822, 0.4466), (0.247, 0.243, 0.261))])
-
 trainTransforms = tv.transforms.Compose([
                  tv.transforms.ToTensor(), 
                  tv.transforms.Normalize(
@@ -36,18 +41,36 @@ trainTransforms = tv.transforms.Compose([
 
 # Here load data
 # TODO: use supervised DataLoader to load images and pass trainTransforms in arg
-dataloader = DataLoader([])
+data_dir = "../comics"
+labels_to_idx = {
+        "Golden Age" : 0,
+        "Silver Age" : 1,
+        "Bronze Age" : 2,
+        "Modern Age" : 3
+    }
+image_datasets = {}
+dataset_full = load_dataset(data_dir, trainTransforms, labels_to_idx)
+
+# Split in train, val and test from the image list
+np.random.seed(42)
+image_datasets["train"], image_datasets["test"] = train_test_split(dataset_full)
+image_datasets["train"], image_datasets["val"] = train_test_split(image_datasets["train"])
 
 # Training
 num_epochs = 5 #you can go for more epochs, I am using a mac
-batch_size = 128
+batch_size = 8
+
+# Create training, validation and test dataloaders
+dataloaders_dict = {x: DataLoader(image_datasets[x], batch_size=batch_size, shuffle=True, num_workers=4) for x in ["train", "val", "test"]}
 
 ae = AutoEncoder().cuda()
 distance = nn.MSELoss()
 optimizer = optim.Adam(ae.parameters(), lr=0.001)
 
+ae.train()
+
 for epoch in range(num_epochs):
-    for data in dataloader:
+    for data in dataloaders_dict["train"]:
         inputs, labels = data
         inputs = Variable(inputs).cuda()
         output = ae(inputs)
